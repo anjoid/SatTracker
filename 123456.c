dc7907231ddb1c79a6bb43e1d54a3c7f6aa82744
anjoid <zhangjing.work@gmail.com>
2014-03-08 13:19:56 +0800

    增加文件
    加了123456.c

---
+++ 123456.c
@@ -0,0 +1,11 @@
+// Read the AD conversion result
+unsigned int read_adc(unsigned char adc_input)
+{
+ADMUX=adc_input|ADC_VREF_TYPE;
+// Start the AD conversion
+ADCSRA|=0x40;
+// Wait for the AD conversion to complete
+while ((ADCSRA & 0x10)==0);
+ADCSRA|=0x10;
+return ADCW;
+}
